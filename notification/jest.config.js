16a38cbf129d35491306f4d08252b0a5b754bcfa
IT21278280 <99583379+IT21278280@users.noreply.github.com>
2025-04-18 15:00:29 +0530

    Removed index.test.js and excluded index.js from coverage

--- notification/jest.config.js
+++ notification/jest.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     testEnvironment: 'node',
     coverageDirectory: 'coverage',
-    collectCoverageFrom: ['**/*.js', '!**/node_modules/**', '!**/coverage/**'],
+    collectCoverageFrom: [
+      '**/*.js',
+      '!**/node_modules/**',
+      '!**/coverage/**',
+      '!jest.config.js',
+      '!index.js', // Exclude index.js
+    ],
   };
